8a1049a8d05997f7917a42d0ad627196eed5b0c6
zhouzhi <zhouzhi1995@hotmail.com>
2019-02-01 19:35:49 +0800

    Update sum.js

--- src/sum.js
+++ src/sum.js
@@ -1,22 +1,22 @@
-import * as Papa from 'papaparse'
+ //Import papaparse for csv -> json, could be avoided if export .json directly from sqlite, however I faced a bit difficulty in doing so
+ //Requires "npm install papaparse" in command line to install this package
+ import * as Papa from 'papaparse'
 
+// Please adjust Token
+Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJhOTEzMWRlMi05YjBlLTQ4YmEtYjcwYS00MzA0MTA5MTZkZWEiLCJpZCI6NTI3NSwic2NvcGVzIjpbImFzciIsImdjIl0sImlhdCI6MTU0Mjg2ODA5MX0.ef3qQCWjP8HqeeYm7VREq0GuezQUNDH9J0ZrQgN6ss4';
 
-    Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJhOTEzMWRlMi05YjBlLTQ4YmEtYjcwYS00MzA0MTA5MTZkZWEiLCJpZCI6NTI3NSwic2NvcGVzIjpbImFzciIsImdjIl0sImlhdCI6MTU0Mjg2ODA5MX0.ef3qQCWjP8HqeeYm7VREq0GuezQUNDH9J0ZrQgN6ss4';
-    var viewer = new Cesium.Viewer("cesiumContainerS", {
+// Create viewer, please adjust the DOM selection
+var viewer = new Cesium.Viewer("cesiumContainerS", {
         terrainProvider: Cesium.createWorldTerrain()
     });
-    // Load Cesium World Terrain
-    // viewer.terrainProvider = Cesium.createWorldTerrain({
-    //     requestWaterMask : true, // required for water effects
-    //     requestVertexNormals : true // required for terrain lighting
-    // });
-    // // Enable depth testing so things behind the terrain disappear.
-    // viewer.scene.globe.depthTestAgainstTerrain = true;
+
     
-    // Create an initial camera view
-    var initialPosition = new Cesium.Cartesian3.fromDegrees(103.799515, 1.060035, 56310.082799425431); // singapore
-    var initialOrientation = new Cesium.HeadingPitchRoll.fromDegrees(3.1077496389876024807, -61.987223091598949054, 0.025883251314954971306);
-    var homeCameraView = {
+// Create an initial camera view
+var initialPosition = new Cesium.Cartesian3.fromDegrees(103.799515, 1.060035, 56310.082799425431); // singapore
+var initialOrientation = new Cesium.HeadingPitchRoll.fromDegrees(3.1077496389876024807, -61.987223091598949054, 0.025883251314954971306);
+
+// Create HomeCarmera
+var homeCameraView = {
         destination : initialPosition,
         orientation : {
             heading : initialOrientation.heading,
@@ -24,6 +24,7 @@ import * as Papa from 'papaparse'
             roll : initialOrientation.roll
         }
     };
+
     // Set the initial view
     viewer.scene.camera.setView(homeCameraView);
     
@@ -32,14 +33,15 @@ import * as Papa from 'papaparse'
     homeCameraView.maximumHeight = 2000;
     homeCameraView.pitchAdjustHeight = 2000;
     homeCameraView.endTransform = Cesium.Matrix4.IDENTITY;
+
     // Override the default home button
     viewer.homeButton.viewModel.command.beforeExecute.addEventListener(function (e) {
         e.cancel = true;
         viewer.scene.camera.flyTo(homeCameraView);
     });
     
-
-   var config = {
+// Create general config settings for parsing csv to json
+var config = {
     delimiter: ",", // auto-detect
     newline: "",    // auto-detect
     quoteChar: '"',
@@ -62,88 +64,128 @@ import * as Papa from 'papaparse'
     withCredentials: undefined,
     transform: undefined
 }
-    var geojsondata = 0
 
+// Create a dummy variable geojsondata
+var geojsondata = 0
+
+// Fetch csv data , Cesium could fetch .geojson directly, thus using a R backend will simplify a lot of the codes below
 fetch('../data/routeCoHaneda.csv')
   .then(response => response.text())
   .then((data) => {
-    geojsondata = {
-  "type": "FeatureCollection",
-  "features":[]}
 
+    // Create the general header in a geojson format, the data information goes under the array corresponding to "features"  
+    geojsondata = {
+        "type": "FeatureCollection",
+        "features":[]}
+
+    // Parse csv to json
     var results = Papa.parse(data,config);
-    console.log(results.data)
-    var final = []
-    var coor = []
-    var pathID = results.data[0].pathID
-    for (var i = 0; i <= results.data.length-1; i++) {
-        
-        if (results.data[i].pathID == pathID){
-            coor.push(JSON.parse(results.data[i].coordinates))
+    
+    // Check json data
+    //console.log(results.data)
+
+    // Here's the json --> geojson conversion (part that goes into backend)
+
+        //Create dummy variables
+        var final = []
+        var coor = []
+
+        //Create the first pathID
+        var pathID = results.data[0].pathID
+
+        //For each entry in the data, 
+        //if pathID is the same as previous, append the coordinate into a single array with pathID as key (thus create polyline)
+        //else change pathID.
+        //this requires pathID to be in sequence, thus ORDER BY in SQLITE is important
+        for (var i = 0; i <= results.data.length-1; i++) {
+            
+            if (results.data[i].pathID == pathID){
+                coor.push(JSON.parse(results.data[i].coordinates))
+            }
+            else{
+                final.push({"type" :  "Feature" , "geometry" : {"type": "LineString","coordinates": coor}, "properties": {"name": results.data[i].pathID}})
+                pathID =  results.data[i].pathID
+                coor= []
+            }
         }
-        else{
-            final.push({"type" :  "Feature" , "geometry" : {"type": "LineString","coordinates": coor}, "properties": {"name": results.data[i].pathID}})
-            pathID =  results.data[i].pathID
-            coor= []
-        }
-    }
-    console.log(final)
-    geojsondata.features = final
-    console.log(geojsondata)
+
+        // Assign the object consists of polyline data to the overall geojson variable with "features" as key
+        geojsondata.features = final
+
+        // Check data
+        //console.log(geojsondata)
 
   })
-  .finally(function(){
-     var geocachePromise = Cesium.GeoJsonDataSource.load(geojsondata)
+
+    // Visualisation for Routes
+    .finally(function(){
+
+    //Set Data
+    var geocachePromise = Cesium.GeoJsonDataSource.load(geojsondata)
     // console.log(geojsondata)
 
     // Add geocache billboard entities to scene and style them
     geocachePromise.then(function(dataSource) {
-    // Add the new data as entities to the viewer
-    // console.log(dataSource)
 
+    //Load Data
     viewer.dataSources.add(dataSource);
 
-    // Get the array of entities
+    // Get the array of entities(polylines)
     var geocacheEntities = dataSource.entities.values;
 
     for (var i = 0; i < geocacheEntities.length; i++) {
         var entity = geocacheEntities[i];
         if (Cesium.defined(entity.polyline)) {
-            // console.log(1)
 
-
+            // 1 random color for each path
             entity.polyline.material = Cesium.Color.fromRandom()
+            // Set width of line
             entity.polyline.width = 2
+
+            // Add granularity, however runs slower and line sometimes dissappear (due to no altitude attribute)
             // entity.polyline.granularity = Cesium.Math.toRadians(0.3)
 
-            // // Add distance display condition
+            // Add distance display condition
             // entity.polyline.distanceDisplayCondition = new Cesium.DistanceDisplayCondition(0, 1000000000.0);
         }
     }
 
 
 
-})}
+    })}
     )
-   var geocachePromise = Cesium.GeoJsonDataSource.load("../data/parsed.geojson")
 
-    // Add geocache billboard entities to scene and style them
-    geocachePromise.then(function(dataSource) {
-    // Add the new data as entities to the viewer
-    viewer.dataSources.add(dataSource);
+    // New dataset for billboard
+    // I parsed this directly using Sqlite and manually typed the overall header for geojson.
+    // Just a different approach
 
-    // Get the array of entities
-    var geocacheEntities = dataSource.entities.values;
+        //Load Data
+        var geocachePromise = Cesium.GeoJsonDataSource.load("../data/parsed.geojson")
 
-    for (var i = 0; i < geocacheEntities.length; i++) {
-        var entity = geocacheEntities[i];
-        if (Cesium.defined(entity.billboard)) {
-            // Adjust the vertical origin so pins sit on terrain
-            entity.billboard.verticalOrigin = Cesium.VerticalOrigin.BOTTOM;
-            // Disable the labels to reduce clutter
-            entity.label = undefined;
-            entity.billboard.color = Cesium.Color.CORAL
-            // Add distance display condition
-            entity.billboard.distanceDisplayCondition = new Cesium.DistanceDisplayCondition(0, 1000000.0);
-        }
-    }})
+        // Add geocache billboard entities to scene and style them
+        geocachePromise.then(function(dataSource) {
+
+        // Add the new data as entities to the viewer
+        viewer.dataSources.add(dataSource);
+
+        // Get the array of entities(billboards)
+        var geocacheEntities = dataSource.entities.values;
+
+        for (var i = 0; i < geocacheEntities.length; i++) {
+            var entity = geocacheEntities[i];
+
+            if (Cesium.defined(entity.billboard)) {
+
+                // Adjust the vertical origin so pins sit on terrain
+                entity.billboard.verticalOrigin = Cesium.VerticalOrigin.BOTTOM;
+
+                // Disable the labels to reduce clutter (enable for WayPoint Names)
+                entity.label = undefined;
+
+                // Set a color
+                entity.billboard.color = Cesium.Color.CORAL
+
+                // Add distance display condition of maximum 1000000 meters (for camera) afterwhich billboards are not shown
+                entity.billboard.distanceDisplayCondition = new Cesium.DistanceDisplayCondition(0, 1000000.0);
+            }
+        }})
